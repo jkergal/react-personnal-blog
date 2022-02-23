7ed9adf1b01da0efae2f46c14298794e30b2b3f9
Johann Kergal <hello@johannkergal.fr>
2022-02-23 01:24:09 +0100

    style (Article) : fix line breaks on articleText parapgraphs with white-space:pre-line on Article.css

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -3,13 +3,13 @@ import { doc, getDoc } from 'firebase/firestore'
 import { db } from '../../firebase.config'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import './Article.css'
 
 export default function Article() {
     const { articleId } = useParams('')
     // console.log(articleId)
     // const [loading, setLoading] = useState(true)
     const [articleData, setArticleData] = useState({})
-    const [articleTextModified, setArticleTextModified] = useState('')
 
     const docRef = doc(db, 'articles', `${articleId}`)
 
@@ -19,8 +19,6 @@ export default function Article() {
                 const docSnap = await getDoc(docRef)
 
                 console.log('response', docSnap)
-
-                // let data = { title: 'not found' }
 
                 if (docSnap.exists()) {
                     console.log('Document data:', docSnap.data())
@@ -36,15 +34,11 @@ export default function Article() {
         fetchData()
     }, [])
 
-    useEffect(() => {
-        setArticleTextModified(articleData.articleText.replace('\\n', '\n'))
-    }, [articleData])
-
     return (
         <div>
             <img src={articleData.bannerUrl}></img>
             <h1>{articleData.title}</h1>
-            <p>{articleTextModified}</p>
+            <p className="article-text">{articleData.articleText}</p>
         </div>
     )
 }
