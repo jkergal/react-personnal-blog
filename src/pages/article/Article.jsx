776a61d4a3d22d8f8148d8db235870c175660b93
Johann Kergal <hello@johannkergal.fr>
2022-02-25 01:43:08 +0100

    feat (Article) : Successfully display article date (converted from firebase timestamp to a string value) on the article page

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -4,65 +4,43 @@ import { db } from '../../firebase.config'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import './Article.css'
-// import { map } from '@firebase/util'
 
 export default function Article() {
     const { articleId } = useParams('')
-    // console.log(articleId)
-    // const [loading, setLoading] = useState(true)
     const [articleData, setArticleData] = useState({})
-    const [firebaseTimestamp, setFirebaseTimestamp] = useState({})
-    // const [convertedTimestamp, setConvertedTimestamp] = useState('')
+    const [articleDateString, setArticleDateString] = useState('')
 
     const docRef = doc(db, 'articles', `${articleId}`)
 
     useEffect(() => {
+        let mounted = true
         const fetchData = async () => {
             try {
                 const docSnap = await getDoc(docRef)
 
                 console.log('response firebase', docSnap)
 
-                if (docSnap.exists()) {
-                    console.log('Document data:', docSnap.data())
+                if (docSnap.exists() && mounted) {
+                    console.log('docSnap.data()', docSnap.data())
+                    setArticleData(docSnap.data())
+                    setArticleDateString(
+                        new Date(docSnap.data().articleDate.seconds * 1000).toDateString()
+                    )
                 }
-
-                setArticleData(docSnap.data())
-                console.log('state data : ' + articleData)
             } catch (err) {
                 console.error(err)
             }
         }
 
         fetchData()
+        return () => (mounted = false)
     }, [])
-
-    useEffect(() => {
-        setFirebaseTimestamp(articleData.articleDate)
-        console.log('setDate')
-    }, [articleData])
-
-    useEffect(() => {
-        console.log(firebaseTimestamp.toDate())
-        // setConvertedTimestamp(
-        //     new Date(firebaseTimestamp.seconds * 1000).toLocaleDateString('en-US')
-        // )
-        // console.log('converted date : ' + convertedTimestamp)
-    }, [firebaseTimestamp])
 
     return (
         <div>
             <img src={articleData.bannerUrl}></img>
             <h1>{articleData.title}</h1>
-            {/* {date.map((dateItem) => (
-                <div key="nanoseconds">{dateItem.nanoseconds}</div>
-            ))} */}
-            {/* <h3>{date}</h3> */}
-            {/* {articleData.map((articleDate) => {
-                ;<h3 key={articleData.id}>
-                    {new Date(item.articleDate.seconds * 1000).toLocaleDateString('en-US')}
-                </h3>
-            })} */}
+            <h3>{articleDateString}</h3>
             <p className="article-text">{articleData.articleText}</p>
         </div>
     )
