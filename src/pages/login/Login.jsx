37874d8ca374ecaef4bd06507c7476613f2b8849
Johann Kergal <hello@johannkergal.fr>
2022-02-25 10:21:45 +0100

    feat (Login) : redirect to Dashboard when already logged in (currentUser)

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Login.css'
 import LoginForm from '../../components/LoginForm'
+import { UserContext } from '../../utils/context/userContext'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+    const { currentUser } = useContext(UserContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (currentUser) {
+            navigate('/private/dashboard')
+        }
+    }, [])
+
     return (
         <div>
             <h1>LOGIN</h1>
