2fcf850ba9bcab5a7c3aa7e0822aae3c729f9fc8
Johann Kergal <hello@johannkergal.fr>
2022-03-07 22:28:37 +0100

    feat (Article) : use PublicArticlesDataCOntext to display article data on the page

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,41 +1,23 @@
 import React from 'react'
-import { doc, getDoc } from 'firebase/firestore'
-import { db } from '../../firebase.config'
 import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import './Article.css'
 import ReactMarkdown from 'react-markdown'
 import '../../utils/style/github-markdown-light.css'
+import { PublicArticlesDataContext } from '../../utils/context/publicArticlesDataContext'
 
 export default function Article() {
     const { articleId } = useParams('')
     const [articleData, setArticleData] = useState({})
     const [articleDateString, setArticleDateString] = useState('')
+    const publicArticles = useContext(PublicArticlesDataContext)
 
-    const docRef = doc(db, 'articles', `${articleId}`)
-
-    useEffect(() => {
-        let mounted = true
-        const fetchData = async () => {
-            try {
-                const docSnap = await getDoc(docRef)
-
-                console.log('response firebase', docSnap)
-
-                if (docSnap.exists() && mounted) {
-                    console.log('docSnap.data()', docSnap.data())
-                    setArticleData(docSnap.data())
-                    setArticleDateString(
-                        new Date(docSnap.data().articleDate.seconds * 1000).toDateString()
-                    )
-                }
-            } catch (err) {
-                console.error(err)
-            }
-        }
-
-        fetchData()
-        return () => (mounted = false)
+    useEffect(async () => {
+        const article = await publicArticles.find(function (post) {
+            if (post.id == articleId) return true
+        })
+        setArticleDateString(new Date(article.articleDate.seconds * 1000).toDateString())
+        setArticleData(article)
     }, [])
 
     return (
