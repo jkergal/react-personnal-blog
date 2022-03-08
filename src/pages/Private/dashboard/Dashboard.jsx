9af57a3d9cec42fa4697d1858009edc9663a9353
Johann Kergal <hello@johannkergal.fr>
2022-03-09 00:33:19 +0100

    feat (Dashboard : drafts) : display drafts list on dashboard

--- src/pages/Private/dashboard/Dashboard.jsx
+++ src/pages/Private/dashboard/Dashboard.jsx
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom'
 import './Dashboard.css'
 import { useNavigate } from 'react-router-dom'
 import { PublicArticlesDataContext } from '../../../utils/context/publicArticlesDataContext'
+import { DraftsDataContext } from '../../../utils/context/drafsDataContext'
 
 export default function Dashboard() {
     const publicArticles = useContext(PublicArticlesDataContext)
+    const drafts = useContext(DraftsDataContext)
 
     const navigate = useNavigate()
 
@@ -24,9 +26,40 @@ export default function Dashboard() {
     return (
         <div className="dashboard-container">
             <h1>Dashboard</h1>
+
             <div className="articles-list-container">
                 <div className="articles-list">
+                    <h3>Public Articles</h3>
                     {publicArticles.map((article) => {
+                        return (
+                            <div key={article.id} className="article-link-wrapper">
+                                <Link to={`/article/${article.id}`}>{article.title}</Link>
+                                <div className="article-buttons-container">
+                                    <button
+                                        className="delete-article-button"
+                                        onClick={() => {
+                                            deleteDocHandler(article.id)
+                                        }}>
+                                        DELETE
+                                    </button>
+                                    <button
+                                        className="edit-article-button"
+                                        onClick={() => {
+                                            editDocHandler(article.id)
+                                        }}>
+                                        EDIT
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+            <div className="articles-list-container">
+                <div className="articles-list">
+                    <h3>Drafts</h3>
+                    {drafts.map((article) => {
                         return (
                             <div key={article.id} className="article-link-wrapper">
                                 <Link to={`/article/${article.id}`}>{article.title}</Link>
