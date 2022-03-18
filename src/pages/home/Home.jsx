28c1dc7a810cde4eb84e77dbe52eb9b362fb9d3d
Johann Kergal <hello@johannkergal.fr>
2022-03-18 10:33:28 +0100

    feat (Header) : create Header.jsx component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import './Home.css'
 import React, { useContext } from 'react'
 import ArticleCard from '../../components/ArticleCard'
 import { FirestoreDataContext } from '../../utils/context/firestoreDataContext'
+import Header from '../../components/Header'
 
 function Home() {
     const { publicArticles } = useContext(FirestoreDataContext)
 
     return (
         <div className="home">
-            <h1>This is home of JK Blog.</h1>
+            <Header pageTitle="Latest articles" />
             <div className="articles-container">
                 <div className="articles-grid">
                     {publicArticles.map((article) => {
