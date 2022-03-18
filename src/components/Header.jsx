2a9e4178fd243d28ea0e8a91cb96efe08efe1ce5
Johann Kergal <hello@johannkergal.fr>
2022-03-18 10:46:01 +0100

    style (Header) : create css of Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import '../utils/style/Header.css'
 
 export default function Header(props) {
-    return <h1>{props.pageTitle}</h1>
+    return (
+        <div className="header">
+            <h1>{props.pageTitle}</h1>
+        </div>
+    )
 }
