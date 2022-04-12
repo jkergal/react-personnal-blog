b01facad5da66834c2cd81faec70d8c017b8bf2c
Johann Kergal <hello@johannkergal.fr>
2022-04-12 23:26:30 +0200

    structure : some minor changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Private from './pages/Private/Private'
 import Dashboard from './pages/Private/dashboard/Dashboard'
-import WriteArticle from './pages/Private/write-article/WriteArticle'
+import WriteArticle from './pages/Private/writeArticle/WriteArticle'
 import Article from './pages/article/Article'
 import Draft from './pages/Private/draft/Draft'
-import EditArticle from './pages/Private/edit-article/EditArticle'
+import EditArticle from './pages/Private/editArticle/EditArticle'
 
 export default function app(props) {
     console.log('App component as launched yeahh')
